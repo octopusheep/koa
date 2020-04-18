ec37fb3969e46f4bc25a2758185eb138c4d6074e
octopusheep <octopusheep@gmail.com>
2020-04-19 01:21:27 +0800

    feat: add verify and register API
    Signed-off-by: octopusheep <octopusheep@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
 
+
 const Koa = require('koa');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 const router = require('koa-router')();
 const app = new Koa();
+
+
+app.use(async (ctx, next)=> {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+      ctx.body = 200; 
+    } else {
+      await next();
+    }
+  });
 
 app.use(bodyParser());
 app.use(router.routes());
--- controllers/login.js
+++ controllers/login.js
@@ -1,56 +1,83 @@
 const Sequelize = require('sequelize');
 const config = require('../config');
 
-// var sequelize = new Sequelize(config.database, config.username, config.password, {
-//     host: config.host,
-//     dialect: 'mysql',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         idle: 30000
-//     }
-// });
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
 
-// var Account = sequelize.define('account', {
-//     sn: {
-//         type: Sequelize.STRING(40),
-//         primaryKey: true
-//     },
-//     username: Sequelize.STRING(40),
-//     password: Sequelize.STRING(40)
+var Account = sequelize.define('account', {
+    sn: {
+        type: Sequelize.STRING(40),
+        primaryKey: true
+    },
+    username: Sequelize.STRING(40),
+    password: Sequelize.STRING(40)
 
-// }, {
-//     timestamps: false
-// });
+}, {
+    timestamps: false
+});
 
 
 var verify_account = async (ctx, next) => {
 
-    await next();
+    // await next();
     var
         login_username = ctx.request.body.username || '',
         login_password = ctx.request.body.password || '';
 
-    // console.log(`login with name: ${login_username}, password: ${login_password}`);
+    console.log(`login with name: ${login_username}, password: ${login_password}`);
 
-    // var accounts = await Account.findAll({
-    //     where: {
-    //         username: login_username,
-    //         password: login_password
-    //     }
-    // });
-    // console.log(`find ${accounts.length} accounts:`);
+    var accounts = await Account.findAll({
+        where: {
+            username: login_username,
+            password: login_password
+        }
+    });
+    console.log(`find ${accounts.length} accounts:`);
 
-    // if (accounts.length == 0) {
-    //     ctx.response.type = 'text/html';
-    //     ctx.response.body = 'verify fail';
-    // } else {
-    //     ctx.response.type = 'text/html';
-    //     ctx.response.body = 'verify success';
-    // }
+    if (accounts.length == 0) {
+        ctx.response.type = 'text/html';
+        ctx.response.body = 'verify fail';
+    } else {
+        ctx.response.type = 'text/html';
+        ctx.response.body = 'verify success';
+    }
+};
+
+var register_account = async (ctx, next) => {
+
+    // await next();
+    var
+        register_username = ctx.request.body.username || '',
+        register_password = ctx.request.body.password || '';
+        register_sn = ctx.request.body.sn || '';
+
+    console.log(`register with name: ${register_username}, password: ${register_password}, sn: ${register_sn}`);
+
+    var account = await Account.create({
+        username: register_username,
+        password: register_password,
+        sn: register_sn
+    });
+    console.log('created: ' + JSON.stringify(account));
+
+    if (account.length == 0) {
+        ctx.response.type = 'text/html';
+        ctx.response.body = 'register fail';
+    } else {
+        ctx.response.type = 'text/html';
+        ctx.response.body = 'register success';
+    }
 };
 
 
 module.exports = {
-    'POST /verify': verify_account
+    'POST /verify': verify_account,
+    'POST /register': register_account
 };
